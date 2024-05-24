d8e5089cb751445964ef084700304d9a6e7c602d
Abhishek Shakya <abshakya63@GMAIL.COM>
2024-05-24 14:06:36 +0530

    Add files via upload

---
+++ DAY 5/LinkedList.c
@@ -0,0 +1,31 @@
+// We are going to learn about linked list.
+
+// A linked list is a linear data structure, in which the elements are not stored at contiguous memory locations.
+
+// The elements in a linked list are linked using pointers.
+
+// Each circle is a node of the linked list, and the number in it is the value of the node.
+
+// The arrow shows the pointers which are used to link the nodes.
+
+// The first node is called the head node, and the last node is called the tail node.
+// The head node is the first node of the linked list.
+// The tail node is the last node of the linked list.
+
+// The Insertion and the Deletion are much easier than array.
+
+
+// Inserion in a linked list is done in two ways:
+// 1. Insertion at the beginning of the linked list
+// 2. Insertion at the end of the linked list
+
+// Deletion in a linked list is done in two ways:
+// 1. Deletion at the beginning of the linked list
+// 2. Deletion at the end of the linked list
+
+#include <stdio.h>
+
+int main(void) {
+  printf("Linked List\n");
+  return 0;
+}
